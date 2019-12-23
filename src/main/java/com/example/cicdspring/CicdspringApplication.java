7ff2bbdde7ab3fb549f342448b62f2a1bd30b4fd
hoanghiep8921 <hoanghiep8921@gmail.com>
2019-12-23 16:51:08 +0700

    update

---
+++ src/main/java/com/example/cicdspring/CicdspringApplication.java
@@ -0,0 +1,21 @@
+package com.example.cicdspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdspringApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Lô cc";
+    }
+
+}
